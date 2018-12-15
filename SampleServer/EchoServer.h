3fe5c988791ea4681fcc09a8c0e57437bc041064
Xu Kai <xukai.sh@gmail.com>
2018-12-15 22:11:58 +0800

    update server

--- SampleServer/EchoServer.h
+++ SampleServer/EchoServer.h
@@ -1,7 +1,9 @@
 #pragma once
 #include <ftd/SocketAcceptor.h>
+#include <ftd/FTD30/PackageCracker.h>
 
-class EchoServer
+using namespace FTD;
+class EchoServer : public FTD::Application, public FTD::PackageCracker
 {
 public:
 	EchoServer(std::string cfgFile);
@@ -9,6 +11,28 @@ public:
 
 	void connect();
 	void disconnect();
+
+	virtual void onCreate(const SessionID&);
+	///connected
+	virtual void onConnect(const SessionID&);
+	//to be disconnected
+	virtual void onDisconnect(const SessionID&);
+	/// Notification of a session successfully logging on
+	virtual void onLogon(const SessionID&);
+	/// Notification of a session logging off or disconnecting
+	virtual void onLogout(const SessionID&);
+	/// Notification of admin message being sent to target
+	virtual void toAdmin(Package&, const SessionID&);
+	/// Notification of app message being sent to target
+	virtual void toApp(Package&, const SessionID&);
+	/// Notification of admin message being received from target
+	virtual void fromAdmin(const Package&, const SessionID&);
+	/// Notification of app message being received from target
+	virtual void fromApp(const Package&, const SessionID&);
+
+	virtual void onHeartBeat();
+
+	virtual void onHeartBeatWarning();
 
 private:
 	std::string m_cfgFile;
