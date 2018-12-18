1b541c613c9a25c1b55bd4768e4bb9ab332e5eed
'xukai' <'xukai@swhysc.com'>
2018-12-18 09:27:22 +0800

    commit

--- auto_gen/auto_gen.py
+++ auto_gen/auto_gen.py
@@ -4,8 +4,8 @@ import os
 path = '../spec/'
 files = ['ftd_3_0.xml',]
 target_path = '../ftd/'
-host_targe_path = '../include/xcp'
-testcase_target_path = '../test_unit'
+host_targe_path = '../swtraderapi/'
+testcase_target_path = '../test_unit/'
 
 project_code = 'Xcp'
 
