d33abcdb6ed82dbf41d69309e429600fab6f75d6
Xu Kai <xukai.sh@gmail.com>
2019-01-23 23:12:37 +0800

    update

--- dbcore/process_order_action.cpp
+++ dbcore/process_order_action.cpp
@@ -1,9 +1,40 @@
 #include "ftdc_all.h"
 
-void processOrderAction(const PlainHeaders& headers, FTD::PackageSPtr pReq, DBWrapper* pWrapper, mco_db_h db)
+using namespace genericdb;
+
+void processOrderActionTransaction(const ReqUserLogin* pReq, mco_trans_h t, RspUserLogin* pRsp);
+
+void processOrderAction(const PlainHeaders& headers, FTD::PackageSPtr pPackage, DBWrapper* pWrapper, mco_db_h db)
 {
 #ifdef _DEBUG
 	std::cout << "[processOrderAction] called\n";
 #endif
+	PlainHeaders rspHeaders = { 0 };
+	rspHeaders.admin_flag = QMSG_FLAG_APP;
+	rspHeaders.msg_type = QMSG_TYPE_RSP;
+	strncpy(rspHeaders.source_queue, headers.target_queue,
+		sizeof(rspHeaders.source_queue));
+	rspHeaders.source_session = headers.source_session;
 
+	std::shared_ptr<RspQryPosition> pRsp =
+		std::make_shared<RspQryPosition>();
+	pRsp->m_sequenceSeries = pPackage->m_sequenceSeries;
+	pRsp->m_sequenceNO = pPackage->m_sequenceNO;
+
+	ReqOrderAction *pReq = (ReqOrderAction*)pPackage.get();
+	pRsp->requestSourceField.RequestID =
+		pReq->inputOrderActionField.RequestID;
+
+	mco_trans_h t = 0;
+	MCO_RET rc = MCO_S_OK;
+
+	rc = mco_trans_start(db, MCO_READ_WRITE, MCO_TRANS_FOREGROUND, &t);
+	if (MCO_S_OK != rc)
+	{
+		pRsp->pErrorField->ErrorCode = FTD_ERROR_CODE_TRANSACTION_ERROR;
+		strcpy(pRsp->pErrorField->ErrorText, FTD_ERROR_TEXT_TRANSACTION_ERROR);
+		pWrapper->uplink(rspHeaders, pRsp);
+		return;
+	}
+	//1
 }
--- dbcore/process_user_login.cpp
+++ dbcore/process_user_login.cpp
@@ -5,16 +5,25 @@ using namespace genericdb;
 
 
 
+void processUserLoginTransaction(const ReqUserLogin* pReq, mco_trans_h t, RspUserLogin* pRsp);
 
-/*
-Package* processUserLogin(const Package* pReq, mco_db_h db)
+void processUserLogin(const PlainHeaders& headers, FTD::PackageSPtr pReq, DBWrapper* pWrapper, mco_db_h db)
 {
-	RspUserLogin *pRsp = new RspUserLogin();
-	const ReqUserLogin* pReqUserLogin = (const ReqUserLogin*)pReq;
+#ifdef _DEBUG
+	std::cout << "[processUserLogin] called\n";
+#endif
+	PlainHeaders rspHeaders = { 0 };
+	rspHeaders.admin_flag = QMSG_FLAG_ADMIN;
+	rspHeaders.msg_type = QMSG_TYPE_RSP;
+	strncpy(rspHeaders.source_queue, headers.target_queue, sizeof(rspHeaders.source_queue));
+	rspHeaders.source_session = headers.source_session;
+	std::shared_ptr<RspUserLogin> pRsp = std::make_shared<RspUserLogin>();
+	const ReqUserLogin* pReqUserLogin = (const ReqUserLogin*)pReq.get();
 	pRsp->rspUserLoginField.BrokerID = pReqUserLogin->reqUserLoginField.BrokerID;
 	pRsp->rspUserLoginField.UserID = pReqUserLogin->reqUserLoginField.UserID;
 	pRsp->pErrorField = CFtdcErrorFieldPtr(new CFtdcErrorField());
 	memset(pRsp->pErrorField.get(), 0, sizeof(CFtdcErrorField));
+	pRsp->rspUserLoginField.HeartbeatInterval = 10;
 	mco_trans_h t = 0;
 	MCO_RET rc = MCO_S_OK;
 	rc = mco_trans_start(db, MCO_READ_WRITE, MCO_TRANS_FOREGROUND, &t);
@@ -22,33 +31,22 @@ Package* processUserLogin(const Package* pReq, mco_db_h db)
 	{
 		pRsp->pErrorField->ErrorCode = FTD_ERROR_CODE_TRANSACTION_ERROR;
 		strcpy(pRsp->pErrorField->ErrorText, FTD_ERROR_TEXT_TRANSACTION_ERROR);
-		return pRsp;
+		pWrapper->uplink(rspHeaders, pRsp);
+		return;
 	}
-
-	User user;
-	rc = User_idx_user_id_find(t, pReqUserLogin->reqUserLoginField.UserID, &user);
-	if (MCO_S_OK != rc)
+	try
 	{
-		pRsp->pErrorField->ErrorCode = FTD_ERROR_CODE_USER_ID_NOT_EXIST;
-		strcpy(pRsp->pErrorField->ErrorText, FTD_ERROR_TEXT_USER_ID_NOT_EXIST);
-		mco_trans_rollback(t);
-		return pRsp;
-
+		processUserLoginTransaction(pReqUserLogin, t, pRsp.get());
+		mco_trans_commit(t);
 	}
-	if (strcmp(((std::string)user.password).c_str(), pReqUserLogin->reqUserLoginField.Password) != 0)
+	catch (MCO::Exception& e)
 	{
-		pRsp->pErrorField->ErrorCode = FTD_ERROR_CODE_PASSWORD_ERROR;
-		strcpy(pRsp->pErrorField->ErrorText, FTD_ERROR_TEXT_PASSWORD_ERROR);
 		mco_trans_rollback(t);
-		return pRsp;
+		pRsp->pErrorField->ErrorCode = e.errorCode;
+		strcpy(pRsp->pErrorField->ErrorText, e.what());
 	}
-
-	pRsp->pErrorField->ErrorCode = 0;
-	strcpy(pRsp->pErrorField->ErrorText, "登录成功");
-	mco_trans_commit(t);
-	return pRsp;
+	pWrapper->uplink(rspHeaders, pRsp);
 }
-*/
 
 void processUserLoginTransaction(const ReqUserLogin* pReq, mco_trans_h t, RspUserLogin* pRsp)
 {
@@ -91,45 +89,4 @@ void processUserLoginTransaction(const ReqUserLogin* pReq, mco_trans_h t, RspUse
 	strcpy(pRsp->rspUserLoginField.TradingDay, ((std::string)config.trade_date).data());
 	pRsp->pErrorField->ErrorCode = 0;
 	strcpy(pRsp->pErrorField->ErrorText, "登录成功");
-}
-
-void processUserLogin(const PlainHeaders& headers, FTD::PackageSPtr pReq, DBWrapper* pWrapper, mco_db_h db)
-{
-#ifdef _DEBUG
-	std::cout << "[processUserLogin] called\n";
-#endif
-	PlainHeaders rspHeaders = { 0 };
-	rspHeaders.admin_flag = QMSG_FLAG_ADMIN;
-	rspHeaders.msg_type = QMSG_TYPE_RSP;
-	strncpy(rspHeaders.source_queue, headers.target_queue, sizeof(rspHeaders.source_queue));
-	rspHeaders.source_session = headers.source_session;
-	std::shared_ptr<RspUserLogin> pRsp = std::make_shared<RspUserLogin>();
-	const ReqUserLogin* pReqUserLogin = (const ReqUserLogin*)pReq.get();
-	pRsp->rspUserLoginField.BrokerID = pReqUserLogin->reqUserLoginField.BrokerID;
-	pRsp->rspUserLoginField.UserID = pReqUserLogin->reqUserLoginField.UserID;
-	pRsp->pErrorField = CFtdcErrorFieldPtr(new CFtdcErrorField());
-	memset(pRsp->pErrorField.get(), 0, sizeof(CFtdcErrorField));
-	pRsp->rspUserLoginField.HeartbeatInterval = 10;
-	mco_trans_h t = 0;
-	MCO_RET rc = MCO_S_OK;
-	rc = mco_trans_start(db, MCO_READ_WRITE, MCO_TRANS_FOREGROUND, &t);
-	if (MCO_S_OK != rc)
-	{
-		pRsp->pErrorField->ErrorCode = FTD_ERROR_CODE_TRANSACTION_ERROR;
-		strcpy(pRsp->pErrorField->ErrorText, FTD_ERROR_TEXT_TRANSACTION_ERROR);
-		pWrapper->uplink(rspHeaders, pRsp);
-		return;
-	}
-	try
-	{
-		processUserLoginTransaction(pReqUserLogin, t, pRsp.get());
-		mco_trans_commit(t);
-	}
-	catch (MCO::Exception& e)
-	{
-		mco_trans_rollback(t);
-		pRsp->pErrorField->ErrorCode = e.errorCode;
-		strcpy(pRsp->pErrorField->ErrorText, e.what());
-	}
-	pWrapper->uplink(rspHeaders, pRsp);
 }
