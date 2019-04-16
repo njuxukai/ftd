f173a9d0b49a849694d4c634cb7c36ec3660079e
'xukai' <'xukai@swhysc.com'>
2019-04-16 17:11:17 +0800

    增加成交回报处理函数

--- dbcore/process_rpt_back_execution_report.cpp
+++ dbcore/process_rpt_back_execution_report.cpp
@@ -5,10 +5,37 @@
 
 using namespace genericdb;
 
+void verifyOriginalOrder(FTD::CFtdcInnerExecutionReportField* pInnerReport);
+void insertToInnerExecutionReport(FTD::CFtdcInnerExecutionReportField* pInnerReport);
+void formatIncExecutionReport();
+void privateBoardcaseIncExecutionReport();
 
 void processRptBackExectionReport(const PlainHeaders& headers, FTD::PackageSPtr pReq, DBWrapper* pWrapper, mco_db_h db)
 {
 #ifdef _DEBUG
 	std::cout << "[processRptBackExecutionReport] called\n";
 #endif
+	bool needPrivatePush = false;
+
+	ReqRptBackExecutionReport* pReqRptBackExecutionReport = (ReqRptBackExecutionReport*)pReq.get();
+	FTD::CFtdcInnerExecutionReportField& innerReport = pReqRptBackExecutionReport->innerExecutionReportField;
+	try
+	{
+		//1 找到原始的委托
+		//2 根据原始委托，填充资金账号等信息后， 确认该成交回报不重复
+
+		//3 插入内部执行报告表
+
+		//4 形成客户执行报告记录，并插入客户执行报告表
+
+		//5 广播客户执行报告
+
+	}
+	catch (dbcore::Exception& e)
+	{
+
+	}
+	catch (McoException& e)
+	{
+	}
 }
